80bc383531c36cdfc4ed575a02c29667f19302f9
vboxuser <it25103000@my.sliit.lk>
2025-10-04 06:13:48 +0000

    created file

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -1,42 +1,24 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-#include<string.h>
 
-//(structure to hold gameinformation typedef struct {
+#define Max_player 3
 
-char**board;//2d array for the game board
-int size;//Board size (N*N)
-int current_player;//current player(0,1 or 2)
-int num_players;//Number of players(2 or 3)
-char symbols[3];//Player symbols('X','O','Z')
-int player_types[3];//0 = human, 1 = computer
-FILE*log_file;//file pointer for logging
-	      
-}
+void displayWelcome();
+void initBoard(char**board,int size);
+void printBoard(char**board,int size);
+int validMove(char**board,int size,int row,int col);
+int cheakWinner(char**board,int size,char mark);
+int boardFilled(char**board,int size);
+void humanTurn(char**board,int size,char mark);
+void computerTurn(char**board,int size,char mark,char opponent);
+int getPlayerType(char symbol);
 
-GameState* initialize_game(int size, int num_players) {
-    GameState* game = malloc(sizeof(GameState));
-    if (!game) {
-        printf("Memory allocation failed!\n");
-        return NULL;
-    }
-    game->board = malloc(size * sizeof(char*));
-    if (!game->board) {
-        printf("Memory allocation failed!\n");
-        free(game);
-        return NULL;
-    }
-     for (int i = 0; i < size; i++) {
-        game->board[i] = malloc(size * sizeof(char));
-        if (!game->board[i]) {
-            printf("Memory allocation failed!\n");
-            // Free previously allocated memory
-            for (int j = 0; j < i; j++) {
-                free(game->board[j]);
-            }
-	    free(game->board);
-            free(game);
-            return NULL;
-        }
+int main() {
+	srand((unsigned int)time(NULL));
 
+	int size,mode,totalPlayer;
+	char symbols[Max_players] = {'X','o','Z'};
+	int playerType[Max_Player]; // 0=human, 1=computer
+        
+	displayWelcome();
